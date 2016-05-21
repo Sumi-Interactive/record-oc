19fcb2bbf6ba2718532b667012d0c7e80d7e5c9c
langker <langker@aliyun.com>
2016-05-21 12:03:25 +0800

    Initial Commit

---
+++ record-oc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  record-oc
+//
+//  Created by langker on 16/5/21.
+//  Copyright © 2016年 langker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ record-oc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  record-oc
+//
+//  Created by langker on 16/5/21.
+//  Copyright © 2016年 langker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
