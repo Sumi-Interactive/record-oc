ab2f42166f3248c06b83568c853014583a29132f
langker <langker@aliyun.com>
2016-05-21 13:10:26 +0800

    refine recordDemo using OC

--- record-oc/ViewController.m
+++ record-oc/ViewController.m
@@ -7,21 +7,75 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import "RecorderManager.h"
+#import "PlayerManager.h"
+#import "SCSiriWaveformView.h"
+@interface ViewController () <RecordingDelegate, PlayingDelegate>
+@property (nonatomic, copy) NSString *filename;
+@property (nonatomic, assign) BOOL isRecording;
+@property (nonatomic, assign) BOOL isPlaying;
 
 @end
+
+SCSiriWaveformView *waveformView;
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initWaveFormView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)record:(id)sender {
+    [RecorderManager sharedManager].delegate = self;
+    [[RecorderManager sharedManager] startRecording];
+}
+- (IBAction)play:(id)sender {
+    [PlayerManager sharedManager].delegate = nil;
+    [[PlayerManager sharedManager] playAudioWithFileName:self.filename delegate:self];
+}
+- (IBAction)stop:(id)sender {
+    [[RecorderManager sharedManager] stopRecording];
+}
+-(void)initWaveFormView {
+    waveformView = [[SCSiriWaveformView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 100)];
+    waveformView.waveColor = [UIColor whiteColor];
+    waveformView.primaryWaveLineWidth = 3.0;
+    waveformView.secondaryWaveLineWidth = 1.0;
+    [self.view addSubview:waveformView];
+}
+#pragma mark - Recording & Playing Delegate
+
+- (void)recordingFinishedWithFileName:(NSString *)filePath time:(NSTimeInterval)interval {
+    self.isRecording = NO;
+    self.filename = filePath;
+}
+
+- (void)recordingTimeout {
+    self.isRecording = NO;
+}
+
+- (void)recordingStopped {
+    self.isRecording = NO;
+}
+
+- (void)recordingFailed:(NSString *)failureInfoString {
+    self.isRecording = NO;
+}
+
+- (void)levelMeterChanged:(float)levelMeter {
+    float normalizedValue = pow(10, levelMeter/20);
+    NSLog(@"%f",levelMeter);
+    [waveformView updateWithLevel:levelMeter];
+}
+
+- (void)playingStoped {
+    self.isPlaying = NO;
 }
 
 @end
